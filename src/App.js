fe3e9ff98aab0989cb834a090282605ceaba84ad
Kyle Dampier <30601856+kyledampier@users.noreply.github.com>
2023-01-27 14:51:40 -0500

    updated .gitignore to ignore secrets

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { openai } from "./lib/OpenAIClient";
 function App() {
   return (
     <div className="App">
-      <h1> Swamphacks React Demo </h1>
+      <h1> Swamphacks - Intro to React </h1>
       {/* <Counter /> */}
       <h2> Counter Demo </h2>
       <Counter color="green" style={{ marginLeft: "0.5em" }} />
